32b8322ebe64150f08b72e0a0795a39a6fff6b71
Greblue <66208385+Greblue@users.noreply.github.com>
2020-11-29 13:10:44 +0700

    Update info.js

--- src/commands/Information/info.js
+++ src/commands/Information/info.js
@@ -2,10 +2,11 @@ const Discord = require('discord.js');
 
 exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
-    .setTitle("<:mdHelp:568466408548335650> Information Centre")
+    .setTitle("Information Centre")
     .setColor("RANDOM")
     .setDescription("This is a quite easy to use which contains: **Moderate** | **Information** | **Fun** & more systems.")
     .setTimestamp()
+    .addField(`**Creator**`, `Greblue#6328`)
     .setFooter(`Requested by ${message.author.tag}`, message.author.displayAvatarURL())
 
     message.channel.send(embed);
